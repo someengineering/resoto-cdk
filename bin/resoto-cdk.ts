4f551edd866ad1d0ea0c5a35c716b75dd55aa3cd
Nikita Melkozerov <nikita@some.engineering>
2022-10-20 18:35:33 +0200

    Use the correct stack description

--- bin/resoto-cdk.ts
+++ bin/resoto-cdk.ts
@@ -6,3 +6,5 @@ import { buildEksBlueprint } from '../lib/eks-stack';
 const app = new cdk.App();
 
 const eksStack = buildEksBlueprint(app, 'resoto-eks-stack');
+// Update the stack description from the blueprint generated one
+eksStack.templateOptions.description = 'EKS cluster with Resoto Helm chart.';
